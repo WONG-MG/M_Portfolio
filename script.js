dbd311b7be987acb8bfb67edadff24c4b82833aa
Qnweii <qnwei.wong@gmail.com>
2023-06-20 10:39:12 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Qualification tab
+var x = document.getElementById('qual_content_edu');
+var y = document.getElementById('qual_content_work');
+
+function myFunction(){
+    x.style.display = 'block';
+    y.style.display = 'none';
+}
+
+function mysecondFunction(){
+    y.style.display = 'block';
+    x.style.display = 'none';
+}
